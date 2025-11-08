903d74156b1e50f2b24820062167ab0f91edb9f1
sxxjxxhxxn <jaehoon572@naver.com>
2025-11-08 18:16:57 +0900

    Initial commit

---
+++ AccessibilityFocuser/AccessibilityFocuserApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityFocuserApp.swift
+//  AccessibilityFocuser
+//
+//  Created by jaehun on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityFocuserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
